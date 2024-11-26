bfb2867e2b1728621744a7d0c828312458a7e3a6
longechipi <castilloacostajean@gmail.com>
2024-11-26 18:56:04 -0400

    Prueba Register Con Dominio

--- model/register/insert_register.php
+++ model/register/insert_register.php
@@ -1,1 +1,24 @@
 <?php
+// Ingresa tu clave secreta.....
+define("RECAPTCHA_V3_SECRET_KEY", '6LfC_4gqAAAAANVt8ZaII_KyOwXdJrynx4iFQIej');
+$token = $_POST['token'];
+$action = $_POST['action'];
+ 
+// call curl to POST request
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_V3_SECRET_KEY, 'response' => $token)));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+curl_close($ch);
+$arrResponse = json_decode($response, true);
+ 
+// verificar la respuesta
+if($arrResponse["success"] == '1' && $arrResponse["action"] == $action && $arrResponse["score"] >= 0.6) {
+    // Si entra aqui, es un humano, puedes procesar el formulario
+	echo "ok!, eres un humano";
+} else {
+    // Si entra aqui, es un robot....
+	echo "Lo siento, parece que eres un Robot";
+}
