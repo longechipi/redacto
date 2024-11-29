fcdf611bfb6a00dc7c3998455b1b5fdb64e3008d
longechipi <castilloacostajean@gmail.com>
2024-11-29 15:35:28 -0400

    env

---
+++ conf/env.php
@@ -0,0 +1,16 @@
+<?php
+//Archivo con Variables de Entorno
+//----------------- VARIABLES DE ENVIO DE CORREO -----------------//
+define('SMTP_HOST', 'mail.wgdigital.com.ve');
+define('SMTP_AUTH', true);
+define('SMTP_USERNAME', 'pruebas@wgdigital.com.ve');
+define('SMTP_PASSWORD', '19341986Chipi**');
+define('SMTP_SECURE', 'ssl');
+define('SMTP_PORT', 465);
+define('FROM_EMAIL', 'pruebas@wgdigital.com.ve');
+define('FROM_NAME', 'TRAMITANDO');
+define('REPLY_TO_EMAIL', 'no-reply@noreply.com');
+define('REPLY_TO_NAME', 'Informacion');
+define('CC_EMAIL', 'castilloacostajean@gmail.com');
+//-----------------FIN VARIABLES DE ENVIO DE CORREO -----------------//
+
